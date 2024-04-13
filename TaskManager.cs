7b720e1d357527d383634035f7532f09399bb24d
zdz437 <zdz437@protonmail.com>
2024-04-13 16:13:17 +0200

    task manager prototype

---
+++ TaskManager.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+
+namespace Productivity
+{
+    internal class TaskManager
+    {
+        private Dictionary<DateTime, List<Task>> tasksByDate;
+        private string filePath;
+
+        public TaskManager(string filePath)
+        {
+            this.filePath = filePath;
+            tasksByDate = LoadTasks();
+        }
+
+        public Dictionary<DateTime, List<Task>> TasksByDate => tasksByDate;
+
+        public void AddTask(DateTime date, Task task)
+        {
+            if (!tasksByDate.ContainsKey(date))
+            {
+                tasksByDate[date] = new List<Task>();
+            }
+            tasksByDate[date].Add(task);
+            SaveTasks();
+        }
+
+        private Dictionary<DateTime, List<Task>> LoadTasks()
+        {
+            if (File.Exists(filePath))
+            {
+                string json = File.ReadAllText(filePath);
+                return JsonConvert.DeserializeObject<Dictionary<DateTime, List<Task>>>(json);
+            }
+            return new Dictionary<DateTime, List<Task>>();
+        }
+
+        private void SaveTasks()
+        {
+            string json = JsonConvert.SerializeObject(tasksByDate, Formatting.Indented);
+            File.WriteAllText(filePath, json);
+        }
+    }
+
+}
